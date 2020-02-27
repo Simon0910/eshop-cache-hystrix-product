0449f8c69a3d83517221c6f0a0d2d61504d7e90a
lzp <15321168632@163.com>
2020-02-27 19:06:31 +0800

    init

---
+++ src/main/java/com/roncoo/eshop/product/ha/Application.java
@@ -0,0 +1,17 @@
+package com.roncoo.eshop.product.ha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
